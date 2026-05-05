968b19878c1a90dc93a5d9d53e2d6231c730947b
Aditya02-28 <princearv2005.email.com>
2026-05-05 16:36:04 +0530

    Fixed overflow issue in reverse integer solution

--- src/Basics/revNumber.java
+++ src/Basics/revNumber.java
@@ -8,6 +8,10 @@ public class revNumber {
 
         while (x != 0) {
             int digit = x % 10;
+            // Overflow check
+            if (rev > Integer.MAX_VALUE / 10 || rev < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
             rev = rev * 10 + digit;
             x = x / 10;
         }
